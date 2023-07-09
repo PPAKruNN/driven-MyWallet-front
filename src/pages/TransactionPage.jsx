e112bdd17c1e260aaef5ebd87e0f4bef52b59177
Pedro Nascimento <sn.jaypee@gmail.com>
2023-07-09 03:06:23 -0300

    add TransactionPage functionallity

--- src/pages/TransactionPage.jsx
+++ src/pages/TransactionPage.jsx
@@ -1,13 +1,42 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { UNSAFE_DataRouterStateContext, useNavigate, useParams } from "react-router-dom"
 import styled from "styled-components"
 
 export default function TransactionsPage() {
+
+  const [value, setValue] = useState("");
+  const [desc, setDesc] = useState("");
+
+  const { tipo } = useParams();
+  const navigate = useNavigate();
+  
+  useEffect( () => {
+    if(tipo !== "entrada" && tipo !== "saida")  navigate("/home");
+  }) 
+
+  function formSubmit(e) {
+    e.preventDefault(); 
+
+    axios.post(`${import.meta.env.VITE_API_URL}/nova-transacao/${tipo}`, {registerLabel: desc, value: parseInt(value)})
+      .then(navigate("/home"))
+      .catch( (reason) => {
+        const status = reason.response.status;
+        if(status === 422) alert("Formato invalido dos dados!");
+        if(status === 401) {
+          alert("Token invalido ou expirado!");
+          navigate("/");
+        }
+      })
+  } 
+
   return (
     <TransactionsContainer>
-      <h1>Nova TRANSAÇÃO</h1>
-      <form>
-        <input placeholder="Valor" type="text"/>
-        <input placeholder="Descrição" type="text" />
-        <button>Salvar TRANSAÇÃO</button>
+      <h1>Nova {tipo}</h1>
+      <form onSubmit={formSubmit}>
+        <input value={value} onChange={e => setValue(e.target.value)} placeholder="Valor" type="number" inputMode="decimal"/>
+        <input value={desc} onChange={e => setDesc(e.target.value)} placeholder="Descrição" type="text"/>
+        <button type="submit">Salvar {tipo} </button>
       </form>
     </TransactionsContainer>
   )
